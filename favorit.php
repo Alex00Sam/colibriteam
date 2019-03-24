507849c8c0a9332c2a3c380c4781e38bda9fad13
DenissK20 <deniska.riga@gmail.com>
2019-03-24 16:31:44 +0200

    update

---
+++ favorit.php
@@ -0,0 +1,20 @@
+<?php
+require 'vendor/autoload.php';
+require 'connection.php';
+
+$app = new \atk4\ui\App('Your favourites');
+$app->initLayout('Centered');
+
+$button = $app->layout->add(['Button','Back on Main', 'green']);
+$button->link(['index']);
+
+$place= new Places($db);
+$place->addCondition('is_favourite',1);
+/*$array[]=new \atk4\data\Model;
+foreach($place as $a){
+  if($a['is_favourite']== true){
+    $array[]=$a;
+  }}
+  */
+$grid=$app->add('Grid');
+$grid->setModel($place);
